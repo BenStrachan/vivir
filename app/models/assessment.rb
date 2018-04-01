f0ce0a9162ee5723daa82ff901d20c3ea175af2a
Ben Strachan <ben@ownerhealth.com.au>
2018-04-01 13:30:50 +1000

    added model

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -3,4 +3,10 @@ class Assessment < ApplicationRecord
   belongs_to :user
   belongs_to :assessment_type
   belongs_to :business
+  has_attached_file :attachment
+validates_attachment_content_type :attachment,
+  content_type: [
+    'application/pdf'
+  ]
+validates_attachment_size :attachment, less_than: 10.megabytes
 end
